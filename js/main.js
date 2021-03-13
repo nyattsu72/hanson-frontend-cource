66a04b3981bf18e53836cc1ea025c5e1487ae1bd
nyattsu72 <75970215+nyattsu72@users.noreply.github.com>
2021-03-14 03:49:27 +0900

    変数名の誤字を修正 変数名の誤字を修正しました。

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 //task01
 const task01Id = document.getElementById("task01");
 
-const neweli = document.createElement("li");
-neweli.textContent = "Task01:このDOMをhtml内のulの中に差し込んでください";
-task01Id.appendChild(neweli);
+const newLi = document.createElement("li");
+newLi.textContent = "Task01:このDOMをhtml内のulの中に差し込んでください";
+task01Id.appendChild(newLi);
