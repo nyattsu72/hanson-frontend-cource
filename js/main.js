673c4d81568d519fb54f47e6b51141710bfdc468
nyattsu72 <75970215+nyattsu72@users.noreply.github.com>
2021-03-14 03:47:16 +0900

    変数名を修正、＋α削除 変数名を動詞＋名詞から名詞に変更。 MSUTの＋αを削除。

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 "static";
 
 //task01
-const getTask01Id = document.getElementById("task01");
+const task01Id = document.getElementById("task01");
 
-const createTask01 = document.createElement("li");
-createTask01.textContent = "Task01:このDOMをhtml内のulの中に差し込んでください";
-getTask01Id.appendChild(createTask01);
-
-//task01 + α
-const getUl = document.getElementsByTagName("ul");
-let ulLength = getUl.length;
-const createLi = document.createElement("li");
-createLi.textContent = "Task01+α:このDOMをhtml内のulの中に差し込んでください";
-for (let i = 0; i < ulLength; i++) {
-  getUl[i].appendChild(createLi);
-  console.log(getUl);
-}
+const neweli = document.createElement("li");
+neweli.textContent = "Task01:このDOMをhtml内のulの中に差し込んでください";
+task01Id.appendChild(neweli);
