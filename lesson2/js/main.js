8d4d0564672c07998214bddad0c06914ce80637a
Seki Natsuko <natsu@seki72ko.site>
2021-03-16 06:17:34 +0900

    feat：課題2の回答　このDOMをJavaScriptでつくり、html内のulの中に差し込んでください

--- lesson2/js/main.js
+++ lesson2/js/main.js
@@ -1,8 +1,23 @@
 "static";
 
 //task01
-const task01Id = document.getElementById("task01");
+const task02Id = document.getElementById("task02");
 
 const newLi = document.createElement("li");
-newLi.textContent = "Task01:このDOMをhtml内のulの中に差し込んでください";
-task01Id.appendChild(newLi);
+
+const newLink = document.createElement('a');
+newLink.href = "1.html";
+newLink.textContent = "これです";
+
+const newBookmarkImage = document.createElement('img');
+newBookmarkImage.src = "/images/bookmark.png";
+newBookmarkImage.alt = "ブックマーク";
+newBookmarkImage.width = 20;
+newBookmarkImage.height = 20;
+
+newLink.appendChild(newBookmarkImage);
+
+
+newLi.appendChild(newLink);
+task02Id.appendChild(newLi);
+
