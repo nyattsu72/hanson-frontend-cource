1ca95d2c9c79d4dd01032dc8b813fdf408e2f07c
Seki Natsuko <natsu@seki72ko.site>
2021-03-17 05:17:46 +0900

    refactor : 3か所、変数名の見直し

--- lesson2/js/main.js
+++ lesson2/js/main.js
@@ -1,23 +1,21 @@
 "static";
 
-//task01
+//task02
 const task02Id = document.getElementById("task02");
 
-const newLi = document.createElement("li");
+const list = document.createElement("li");
 
-const newLink = document.createElement('a');
-newLink.href = "1.html";
-newLink.textContent = "これです";
+const link = document.createElement('a');
+link.href = "1.html";
+link.textContent = "これです";
 
-const newBookmarkImage = document.createElement('img');
-newBookmarkImage.src = "/images/bookmark.png";
-newBookmarkImage.alt = "ブックマーク";
-newBookmarkImage.width = 20;
-newBookmarkImage.height = 20;
+const image = document.createElement('img');
+image.src = "/images/bookmark.png";
+image.alt = "ブックマーク";
+image.width = 20;
+image.height = 20;
 
-newLink.appendChild(newBookmarkImage);
-
-
-newLi.appendChild(newLink);
-task02Id.appendChild(newLi);
+link.appendChild(image);
+list.appendChild(link);
+task02Id.appendChild(list);
 
