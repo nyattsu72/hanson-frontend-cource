ff7d8fff68d510fb9b488ab0f9b1e5caebee91f6
Seki Natsuko <natsu@seki72ko.site>
2021-04-15 05:37:56 +0900

    feat : Load JSON faile.

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,44 +1,12 @@
-'use strict';
-const url = 'http://designtorch.xsrv.jp/demo/json/listdata.json';
 //Jsonファイル読み込み
+const requestURL = "https://designtorch.xsrv.jp/demo/json/listdata.json";
+const request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
 
-fetch(url)
-  .then((response) => {
-  return response.json()
-  })
-  .then((result) => {
-    linkList(result);
-  })
-  .catch((e) => {
-    alert('Data read failure');
-  })
+request.onload = () => {
+  const pageList = JSON.parse(request.response);
+  console.log(pageList.to);
+};
 
-//DOM操作
-
-  const listsMenu = document.getElementById('lists-menu');
-  const createdList = document.createElement('ul');
-
-function linklist(jsondata) {
-  jsondata.forEach(item => {
-    const createdListItem = document.createElement('li');
-
-    const createdImg = document.createElement('img');
-    createdImg.src = item.img;
-    createdImg.alt = item.alt;
-    createdImg.width = 40;
-    createdImg.height = 40;
-
-    const createdAnchor = document.createElement('a');
-    createdAnchor.href = item.to;
-    createdAnchor.textContent = item.text;
-
-    createdAnchor.insertAdjacentElement('afterbegin', createdImg);
-    listsMenu.appendChild(createdList).appendChild(createdListItem).appendChild(createdAnchor);
-  });
-}
-
-
-//非同期処理
-
-
-
+request.send();
