4e965d8c2d52e652cf4bc45a7b778eab2d1edbab
Seki Natsuko <natsu@seki72ko.site>
2021-04-17 07:05:03 +0900

    feat : Add Promise to Task4.

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,12 +1,28 @@
-//Jsonファイル読み込み
-const requestURL = "https://designtorch.xsrv.jp/demo/json/listdata.json";
-const request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
+'use strict';
+const listsMenu = document.getElementById('lists-menu');
+const createdList = document.createElement('ul');
+const menuContents = [{ to: "bookmark.html", img: '1.png', alt: '画像1', text: 'ブックマーク' },
+  { to: 'message.html', img: '2.png', alt: '画像2', text: 'メッセージ' }];
 
-request.onload = () => {
-  const pageList = JSON.parse(request.response);
-  console.log(pageList.to);
-};
+const gatedMenuContents = new Promise((resolve) => {
+  resolve(menuContents);
+});
 
-request.send();
+gatedMenuContents.then((value) => {
+  value.forEach(item => {
+    const createdListItem = document.createElement('li');
+
+    const createdImg = document.createElement('img');
+    createdImg.src = item.img;
+    createdImg.alt = item.alt;
+    createdImg.width = 40;
+    createdImg.height = 40;
+
+    const createdAnchor = document.createElement('a');
+    createdAnchor.href = item.to;
+    createdAnchor.textContent = item.text;
+
+    createdAnchor.insertAdjacentElement('afterbegin', createdImg);
+    listsMenu.appendChild(createdList).appendChild(createdListItem).appendChild(createdAnchor);
+  });
+})
