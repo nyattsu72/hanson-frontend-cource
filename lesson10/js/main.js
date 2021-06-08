6229a67d819b12f5be34fba576172d795c1847ce
Seki Natsuko <natsu@seki72ko.site>
2021-06-09 06:13:33 +0900

    feat : add try-catch-finaly

---
+++ lesson10/js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+const menuContainer = document.getElementById('js-menu_container');
+const menuLists = document.createElement('ul');
+const menuContents = [{ to: "bookmark.html", img: 'images/1.png', alt: '画像1', text: 'ブックマーク' },
+  { to: 'message.html', img: 'images/2.png', alt: '画像2', text: 'メッセージ' }];
+const loading = document.getElementById('js-loading');
+
+const loadingImg = document.createElement('img');
+loadingImg.src = "images/loading-circle.gif";
+loadingImg.classList.add('el_loadingImg');
+
+function createMenuList(values) {
+  
+  values.forEach(value => {    
+      const listItem = document.createElement('li');
+
+      const listImg = document.createElement('img');
+      listImg.src = value.img;
+      listImg.alt = value.alt;
+      listImg.width = 40;
+      listImg.height = 40;
+
+      const listAnchor = document.createElement('a');
+      listAnchor.href = value.to;
+      listAnchor.textContent = value.text;
+    
+      const fragment = document.createDocumentFragment();
+      fragment.appendChild(listAnchor).appendChild(listImg);
+      listItem.appendChild(fragment);
+
+      menuContainer.appendChild(menuLists).appendChild(listItem);
+    });
+}
+
+const fetchMenuContents = new Promise((resolve, reject) => {
+  loading.appendChild(loadingImg);
+  setTimeout(() => {
+    resolve(menuContents);
+    reject('Could not get the value');
+    }, 3000);
+});
+
+async function callMenuContents() {
+  try {
+    const menuContentsValues = await fetchMenuContents;
+    return menuContentsValues;
+  } catch (e) {
+    console.error("error：", e.message);
+  } finally {
+  console.log('completed');
+  loading.remove();
+  createMenuList(menuContentsValues);
+  }
+}
+
+callMenuContents();
+
+
