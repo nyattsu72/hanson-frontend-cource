c008d3253cdf738e7d0cc24089e43edc8e341f94
nyattsu72 <nyattsu72@gmail.com>
2021-08-17 07:00:04 +0900

    fix:Fixed array processing of createMenuList function.

--- lesson11/js/main.js
+++ lesson11/js/main.js
@@ -14,20 +14,23 @@ function showLoadingImg() {
 function createMenuList(values) {
   const fragment = document.createDocumentFragment();
 
-  values.forEach((value) => {
-    const listItem = document.createElement("li");
+  Object.keys(values).forEach((key) => {
+    values[key].forEach(value => {
+      const listItem = document.createElement("li");
+  
+      const listImg = document.createElement("img");
+      listImg.src = value.img;
+      listImg.alt = value.alt;
+      listImg.width = 40;
+      listImg.height = 40;
+  
+      const listAnchor = document.createElement("a");
+      listAnchor.href = value.a;
+      listAnchor.textContent = value.text;
+  
+      fragment.appendChild(listItem).appendChild(listAnchor).appendChild(listImg);
 
-    const listImg = document.createElement("img");
-    listImg.src = value.img;
-    listImg.alt = value.alt;
-    listImg.width = 40;
-    listImg.height = 40;
-
-    const listAnchor = document.createElement("a");
-    listAnchor.href = value.a;
-    listAnchor.textContent = value.text;
-
-    fragment.appendChild(listItem).appendChild(listAnchor).appendChild(listImg);
+    })
   });
 
   menuContainer.appendChild(menuLists).appendChild(fragment);
@@ -36,13 +39,13 @@ function createMenuList(values) {
 async function fetchMenuContents(){
   showLoadingImg();
 
-  const menuDataURL = 'https://jsondata.okiba.me/v1/json/78xRb210814201731';
+  const menuDataURL = 'https://jsondata.okiba.me/v1/json/Aticp210816214718';
   const response = await fetch(menuDataURL);
 
   console.log(response.status);
 
   if(response.status === 200){
-    return await response.json()
+    return await response.json();
   }else{
    console.error("Could not get the value");
   }
@@ -50,16 +53,18 @@ async function fetchMenuContents(){
 }
 
 async function callMenuContents() {
-  // try {
-    const menuContentsValues = await fetchMenuContents();
-    console.log(menuContentsValues);
-    createMenuList(menuContentsValues);
+  try {
+    const menuContents = await fetchMenuContents();
+    console.log(menuContents);
+    createMenuList(menuContents);
 
-  // } catch (e) {
-  //   menuContainer.textContent = "表示することができませんでした";
-  // } finally {
-  //   loading.remove();
-  // }
+    
+
+  } catch (e) {
+    menuContainer.textContent = "表示することができませんでした";
+  } finally {
+    loading.remove();
+  }
 }
 
 callMenuContents();
