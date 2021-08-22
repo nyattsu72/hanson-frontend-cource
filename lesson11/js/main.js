b5a3c2e55af358b61f64737b02fbd9492456b832
nyattsu72 <nyattsu72@gmail.com>
2021-08-23 05:39:33 +0900

    refactor: Change fetchMenuContents function from if to try-catch

--- lesson11/js/main.js
+++ lesson11/js/main.js
@@ -7,61 +7,53 @@ function showLoadingImg() {
   const loadingImg = document.createElement("img");
   loadingImg.src = "images/loading-circle.gif";
   loadingImg.classList.add("el_loadingImg");
-
   loading.appendChild(loadingImg);
 }
 
-function createMenuList({data}) {
+function createMenuList(data) {
   const fragment = document.createDocumentFragment();
 
   data.forEach((value) => {
-      const listItem = document.createElement("li");
-  
-      const listImg = document.createElement("img");
-      listImg.src = value.img;
-      listImg.alt = value.alt;
-      listImg.width = 40;
-      listImg.height = 40;
-  
-      const listAnchor = document.createElement("a");
-      listAnchor.href = value.a;
-      listAnchor.textContent = value.text;
-  
-      fragment.appendChild(listItem).appendChild(listAnchor).appendChild(listImg);
+    const listItem = document.createElement("li");
+    const listImg = document.createElement("img");
+    listImg.src = value.img;
+    listImg.alt = value.alt;
+    listImg.width = 40;
+    listImg.height = 40;
 
-    });
-
+    const listAnchor = document.createElement("a");
+    listAnchor.href = value.a;
+    listAnchor.textContent = value.text;
+    fragment.appendChild(listItem).appendChild(listAnchor).appendChild(listImg);
+  });
   menuContainer.appendChild(menuLists).appendChild(fragment);
 }
 
-const MENU_DATA_URL = 'https://jsondata.okiba.me/v1/json/Aticp210816214718';
+const MENU_DATA_URL = "https://jsondata.okiba.me/v1/json/Aticp210816214718";
 
-async function fetchMenuContents(){
+async function fetchMenuContents() {
   showLoadingImg();
-
-  const response = await fetch(MENU_DATA_URL);
-  
-  if(response.status === 200){
-    return await response.json();
-
-  }else{
-   console.error("Could not get the value");
+  try {
+    const response = await fetch(MENU_DATA_URL);
+    const json = await response.json();
+    return json.data;
+  } catch {
+    console.error("error： HTTP error, status = ", +response.status);
   }
-
 }
 
 async function callMenuContents() {
   try {
     const menuContents = await fetchMenuContents();
-    createMenuList(menuContents);
-
+    if (menuContents.length > 0) {
+      createMenuList(menuContents);
+    } else {
+      menuContainer.textContent = "表示するメニューがありませんでした";
+    }
   } catch {
     menuContainer.textContent = "表示することができませんでした";
-    console.error("Could not get the value");
-
   } finally {
     loading.remove();
-    
   }
 }
 
