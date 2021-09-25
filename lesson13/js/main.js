2291c07aac3b503069f6812b2dc3ae7f52e9bbf8
nyattsu <seki72ko@gmail.com>
2021-09-25 23:28:32 +0900

    fix: Click to open a modal window.

--- lesson13/js/main.js
+++ lesson13/js/main.js
@@ -1,95 +1,146 @@
 'use strict';
-const menuContainer = document.getElementById('js-menu_container');
-const menuLists = document.createElement('ul');
+//Modal
+const modalContainer = document.getElementById('js-modalContainer');
+const modalBg = document.createElement('div');
+const modalWindow = document.createElement('div');
 
-function showLoadingImg() {
-	const createLoadingBox = document.createElement('div');
-	createLoadingBox.classList.add('ly_loading');
-	createLoadingBox.id = 'js-loading';
-	const loadingImg = document.createElement('img');
-	loadingImg.src = 'images/loading-circle.gif';
-	loadingImg.classList.add('el_loadingImg');
-	menuContainer.appendChild(createLoadingBox).appendChild(loadingImg);
+function createModalWindow() {
+	const modal = document.createDocumentFragment();
+	const createMenuBtn = document.createElement('button');
+	const createCloseBtn = document.createElement('button');
+	const createMenuContainer = document.createElement('div');
+
+	//modal container
+	modalWindow.classList.add('bl_modal');
+	modalWindow.setAttribute('id', 'js-modalContainer');
+
+	//menu button
+	createMenuBtn.setAttribute('id', 'js-loadmenuBtn');
+	createMenuBtn.setAttribute('class', 'btn');
+	createMenuBtn.textContent = `MENU`;
+
+	//close button
+	createCloseBtn.setAttribute('id', 'js-closeModal');
+	createCloseBtn.setAttribute('class', 'el_closeModalBtn');
+	createCloseBtn.textContent = '×';
+
+	createMenuContainer.setAttribute('id', 'js-menu_container');
+
+	modal.appendChild(createMenuBtn);
+	modal.appendChild(createCloseBtn);
+	modal.appendChild(createMenuContainer);
+	modalWindow.appendChild(modal);
+	document.body.appendChild(modalWindow);
+
+	const clickBtn = document.getElementById('js-loadmenuBtn');
+	clickBtn.addEventListener(
+		'click',
+		(event) => {
+			callMenuContents();
+			event.stopPropagation();
+		},
+		false
+	);
+
+	const closeModlBtn = document.getElementById('js-closeModal');
+
+	closeModlBtn.addEventListener(
+		'click',
+		() => {
+			removeInnerHTML(modalWindow);
+			removeInnerHTML(menuLists);
+			document.body.removeAttribute('style');
+			modalBg.remove();
+			modalWindow.remove();
+		},
+		false
+	);
+
+	//CREATE MENU contents
+	const menuContainer = document.getElementById('js-menu_container');
+	const menuLists = document.createElement('ul');
+
+	function showLoadingImg() {
+		const createLoadingBox = document.createElement('div');
+		createLoadingBox.classList.add('ly_loading');
+		createLoadingBox.id = 'js-loading';
+		const loadingImg = document.createElement('img');
+		loadingImg.src = 'images/loading-circle.gif';
+		loadingImg.classList.add('el_loadingImg');
+		menuContainer.appendChild(createLoadingBox).appendChild(loadingImg);
+	}
+
+	function hideLoadingImg() {
+		const loading = document.getElementById('js-loading');
+		loading.remove();
+	}
+
+	function removeInnerHTML(element) {
+		element.innerHTML = '';
+	}
+
+	//menulist
+	function createMenuList(data) {
+		const fragment = document.createDocumentFragment();
+		removeInnerHTML(menuLists);
+
+		data.forEach((value) => {
+			const listItem = document.createElement('li');
+			const listImg = document.createElement('img');
+			listImg.src = value.img;
+			listImg.alt = value.alt;
+			listImg.width = 40;
+			listImg.height = 40;
+
+			const listAnchor = document.createElement('a');
+			listAnchor.href = value.a;
+			listAnchor.textContent = value.text;
+			fragment
+				.appendChild(listItem)
+				.appendChild(listAnchor)
+				.appendChild(listImg);
+		});
+		menuContainer.appendChild(menuLists).appendChild(fragment);
+	}
+
+	const MENU_DATA_URL = 'https://jsondata.okiba.me/v1/json/Aticp210816214718';
+	async function fetchMenuContents() {
+		showLoadingImg();
+		try {
+			const response = await fetch(MENU_DATA_URL);
+			const json = await response.json();
+			return json.data;
+		} catch {
+			console.error('Could not get the value');
+		}
+	}
+
+	async function callMenuContents() {
+		try {
+			const menuContents = await fetchMenuContents();
+			if (menuContents.length > 0) {
+				createMenuList(menuContents);
+			} else {
+				menuContainer.textContent = '表示するメニューがありませんでした';
+			}
+		} catch {
+			menuContainer.textContent = '表示することができませんでした';
+		} finally {
+			hideLoadingImg();
+		}
+	}
 }
 
-function hideLoadingImg() {
-	const loading = document.getElementById('js-loading');
-	loading.remove();
-}
+//open Modal
+const openModalBtn = document.getElementById('js-openModalBtn');
 
-function removeInnerHTML(element) {
-	element.innerHTML = '';
-}
-
-function createMenuList(data) {
-	const fragment = document.createDocumentFragment();
-	removeInnerHTML(menuLists);
-
-	data.forEach((value) => {
-		const listItem = document.createElement('li');
-		const listImg = document.createElement('img');
-		listImg.src = value.img;
-		listImg.alt = value.alt;
-		listImg.width = 40;
-		listImg.height = 40;
-
-		const listAnchor = document.createElement('a');
-		listAnchor.href = value.a;
-		listAnchor.textContent = value.text;
-		fragment.appendChild(listItem).appendChild(listAnchor).appendChild(listImg);
-	});
-	menuContainer.appendChild(menuLists).appendChild(fragment);
-}
-
-const clickBtn = document.getElementById('js-loadmenuBtn');
-clickBtn.addEventListener(
+openModalBtn.addEventListener(
 	'click',
 	() => {
-		callMenuContents();
+		modalBg.classList.add('el_modalBg');
+		document.body.appendChild(modalBg);
+		createModalWindow();
+		document.body.style.position = 'fixed';
 	},
 	false
 );
-
-const MENU_DATA_URL = 'https://jsondata.okiba.me/v1/json/Aticp210816214718';
-async function fetchMenuContents() {
-	showLoadingImg();
-	try {
-		const response = await fetch(MENU_DATA_URL);
-		const json = await response.json();
-		return json.data;
-	} catch {
-		console.error('Could not get the value');
-	}
-}
-
-async function callMenuContents() {
-	try {
-		const menuContents = await fetchMenuContents();
-		if (menuContents.length > 0) {
-			createMenuList(menuContents);
-		} else {
-			menuContainer.textContent = '表示するメニューがありませんでした';
-		}
-	} catch {
-		menuContainer.textContent = '表示することができませんでした';
-	} finally {
-		hideLoadingImg();
-	}
-}
-
-//Modal
-const openModalBtn = document.getElementById('js-openModalBtn');
-const modalContainer = document.getElementById('js-modalContainer');
-const closeModlBtn = document.getElementById('js-closeModal');
-const modalBg = document.createElement('div');
-
-openModalBtn.addEventListener('click', () => {
-	modalContainer.style.visibility = 'visible';
-	modalContainer.style.opacity = '1';
-});
-
-closeModlBtn.addEventListener('click', () => {
-	modalContainer.style.visibility = 'hidden';
-	modalContainer.style.opacity = '0';
-	removeInnerHTML(menuLists);
-});
