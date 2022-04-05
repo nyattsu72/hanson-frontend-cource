5c2301003f90afd6fd1698f10098d1693151decc
nyattsu72 <seki72ko@gmail.com>
2022-04-05 11:50:00 +0900

    add:Added article loading process by switching tabs.

--- lesson16/js/main.js
+++ lesson16/js/main.js
@@ -1,12 +1,18 @@
 'use strict';
 
-function renderCategoryTab(data){
-	console.log(data);
-	const categories = data.category;
-	const fragment = document.createDocumentFragment();
-	console.log(categories);
+const categoryTab = document.getElementById('js-tab');
 
-	categories.forEach((category) => {
+function createTabContent(){
+	const newsContentArea = document.createElement('div');
+	newsContentArea.classList.add('news-area');
+	newsContentArea.setAttribute('role','tabpanel');
+	categoryTab.parentNode.insertBefore(newsContentArea,categoryTab.nextSibling);
+}
+
+function renderCategoryTab(data){
+	const fragment = document.createDocumentFragment();
+
+	data.forEach((category) => {
 		const categoryTab = document.createElement('li');
 		categoryTab.classList.add('tab_list_item');
 		categoryTab.setAttribute('role','presentation');
@@ -31,10 +37,10 @@ function categoryTabInitial(){
 }
 
 function renderNewsArticle(data){
-	const article = data.article;
-	const articleRenderArea = document.querySelector('.news-list');
+	const newsContentArea = document.querySelector('.news-area');
+	const newsLists = document.createElement('ul');
+	newsLists.classList.add('news-list');
 	const fragment = document.createDocumentFragment();
-	console.log(article);
 
 	const displayedResults = 5;
 	for (let i = 0; i < displayedResults; ++i){
@@ -43,15 +49,18 @@ function renderNewsArticle(data){
 
 		const anchor = document.createElement('a');
 		anchor.classList.add('news-link');
-		anchor.href = article[i].link;
-		anchor.textContent = article[i].title;
+		anchor.href = data[i].link;
+		anchor.textContent = data[i].title;
 		fragment.appendChild(li).appendChild(anchor);
 	}
-	console.log(fragment);
-	articleRenderArea.appendChild(fragment);
+
+	newsContentArea.appendChild(newsLists).appendChild(fragment);
+};
+
+function resetArticle(){
+	const newsLists= document.querySelector('.news-list');
+	newsLists.remove();
 }
-
-
 
 function displayErrorMassage(error){
 	const getNewsArea = document.querySelectorAll('.news-area');
@@ -79,16 +88,76 @@ async function fetchNewsData() {
 	}
 }
 
-async function callNewsArticle(){
-	const articleData = await fetchNewsData();
-	try{
-		renderCategoryTab(articleData);
+async function callCategry(){
+	const data = await fetchNewsData();
+	const categoryData = data.category;
+
+	if(categoryData){
+		renderCategoryTab(categoryData);
 		categoryTabInitial()
+	}
+}
+
+async function callAllArticle(){
+	const data = await fetchNewsData();
+	const articleData = data.article;
+
+	try{
 		renderNewsArticle(articleData);
-		console.log(articleData);
 	} catch (error) {
 		displayErrorMassage(error);
 	}
 }
 
-callNewsArticle();
+async function callSerectArticle(e){
+	const data = await fetchNewsData();
+	const articleData = data.article;
+
+	const selectedTab = e.target.textContent;
+
+	const sortData = articleData.filter((filterData) => {
+		return filterData.category === selectedTab;
+	});
+	renderNewsArticle(sortData);
+}
+
+
+function init(){
+	createTabContent()
+	callCategry();
+	callAllArticle()
+}
+
+init();
+
+function changeTabs(e) {
+	const tabs = document.querySelectorAll('[role="tab"]');
+	const selectedTab = e.target;
+	tabs.forEach((target) => target.setAttribute('aria-selected', false));
+	selectedTab.setAttribute('aria-selected', true);
+
+	const selectCategory = selectedTab.textContent;
+
+	if(selectCategory === 'ニュース'){
+		resetArticle()
+		callAllArticle()
+	}else{
+		resetArticle()
+		callSerectArticle(e);
+	}
+};
+
+const categorySortArticle = (data) => {
+	const categoryTab = document.getElementById('js-tab');
+	const selectCategoryTab = categoryTab.querySelector('.tab-button[aria-selected="true"]').textContent;
+
+	return data.filter((filterData) => {
+		return filterData.category === selectCategoryTab;
+	});
+};
+
+categoryTab.addEventListener('click', (e) => {
+	changeTabs(e)
+})
+
+
