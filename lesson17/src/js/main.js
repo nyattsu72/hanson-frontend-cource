01bbb881fdc25938bc6aeeee3fa90ad77c4d4a94
nyattsu72 <seki72ko@gmail.com>
2022-04-29 07:31:33 +0900

    fix:Corrected the form to check the equality up to the direction + corrected the function name of the error message.

--- lesson17/src/js/main.js
+++ lesson17/src/js/main.js
@@ -1,59 +1,59 @@
-function showLoadingImage(){
-  const main = document.getElementById('main');
+function showLoadingImage() {
+  const main = document.getElementById("main");
   const fragment = document.createDocumentFragment();
-  const renderLoadingBox = document.createElement('div');
-  renderLoadingBox.classList.add('loading');
-  renderLoadingBox.id = 'js-loading';
-  const loadingImage = document.createElement('img');
-  loadingImage.src = 'images/loading.gif';
+  const renderLoadingBox = document.createElement("div");
+  renderLoadingBox.classList.add("loading");
+  renderLoadingBox.id = "js-loading";
+  const loadingImage = document.createElement("img");
+  loadingImage.src = "images/loading.gif";
   loadingImage.width = 80;
   loadingImage.height = 80;
-  loadingImage.classList.add('loading__image');
+  loadingImage.classList.add("loading__image");
   fragment.appendChild(renderLoadingBox).appendChild(loadingImage);
   document.body.insertBefore(fragment, main);
 }
 
-function removeLoading(){
-  document.getElementById('js-loading').remove();
+function removeLoading() {
+  document.getElementById("js-loading").remove();
 }
 
-function displayErrorMassage(error){
-  const slideImageArea = document.querySelector('.mainvisual__images__inner');
-  const createTextBox = document.createElement('p');
-  createTextBox.classList.add('');
+function displayErrorMassage(error) {
+  const slideImageArea = document.querySelector(".mainvisual__images__inner");
+  const createTextBox = document.createElement("p");
+  createTextBox.classList.add("");
   createTextBox.textContent = error;
   slideImageArea.appendChild(createTextBox);
 }
 
 function renderSlideArea() {
-  const slideImageArea = document.createElement('div');
-  slideImageArea.classList.add('mainvisual__images__inner');
+  const slideImageArea = document.createElement("div");
+  slideImageArea.classList.add("mainvisual__images__inner");
 
-  const sliderArea = document.querySelector('.mainvisual__images');
+  const sliderArea = document.querySelector(".mainvisual__images");
 
   sliderArea.appendChild(slideImageArea);
 }
 
-function renderSlideImage(item){
-  const slideImageArea = document.querySelector('.mainvisual__images__inner');
+function renderSlideImage(item) {
+  const slideImageArea = document.querySelector(".mainvisual__images__inner");
 
-  const slider = document.createElement('ul');
-  slider.classList.add('slider');
+  const slider = document.createElement("ul");
+  slider.classList.add("slider");
 
   const fragment = document.createDocumentFragment();
-  item.forEach((image,i) => {
-    const sliderItem = document.createElement('li');
-    sliderItem.classList.add('slider__item');
-    sliderItem.setAttribute('data-slide-index', i+1);
+  item.forEach((image, i) => {
+    const sliderItem = document.createElement("li");
+    sliderItem.classList.add("slider__item");
+    sliderItem.setAttribute("data-slide-index", i + 1);
 
-    const sliderImage = document.createElement('img');
-    sliderImage.classList.add('slider__image');
+    const sliderImage = document.createElement("img");
+    sliderImage.classList.add("slider__image");
     sliderImage.src = image.image;
     sliderImage.width = image.width;
     sliderImage.height = image.height;
 
-    if(i == 0 ) {
-      sliderItem.classList.add('is-active')
+    if (i === 0) {
+      sliderItem.classList.add("is-active");
     }
 
     fragment.appendChild(sliderItem).appendChild(sliderImage);
@@ -62,149 +62,147 @@ function renderSlideImage(item){
   slideImageArea.appendChild(slider).appendChild(fragment);
 }
 
-function renderSlideButton(){
+function renderSlideButton() {
   let fragment = document.createDocumentFragment();
 
-  const buttonArea = document.createElement('div');
-  buttonArea.classList.add('slider__button');
+  const buttonArea = document.createElement("div");
+  buttonArea.classList.add("slider__button");
 
-  const prevButton = document.createElement('button');
-  prevButton.setAttribute('class', 'arrow prev');
-  prevButton.id = 'js-button_prev';
-  prevButton.setAttribute('aria-label','previous');
-  prevButton.textContent = 'previous';
+  const prevButton = document.createElement("button");
+  prevButton.setAttribute("class", "arrow prev");
+  prevButton.id = "js-button_prev";
+  prevButton.setAttribute("aria-label", "previous");
+  prevButton.textContent = "previous";
 
   fragment.appendChild(prevButton);
 
-  const nextButton = document.createElement('button');
-  nextButton.setAttribute('class','arrow next');
-  nextButton.id = 'js-button_next';
-  nextButton.textContent = 'next';
-  nextButton.setAttribute('aria-label','next');
-
+  const nextButton = document.createElement("button");
+  nextButton.setAttribute("class", "arrow next");
+  nextButton.id = "js-button_next";
+  nextButton.textContent = "next";
+  nextButton.setAttribute("aria-label", "next");
 
   fragment.appendChild(nextButton);
 
-  const getSlideArea = document.querySelector('.mainvisual__images');
+  const getSlideArea = document.querySelector(".mainvisual__images");
 
   getSlideArea.appendChild(buttonArea).appendChild(fragment);
-
 }
 
-function renderPagenation(item){
-  const pagenation = document.createElement('div');
-  pagenation.classList.add('slider__pagination');
+function renderPagenation(item) {
+  const pagenation = document.createElement("div");
+  pagenation.classList.add("slider__pagination");
 
   let fragment = document.createDocumentFragment();
 
-  const current = document.createElement('span');
-  current.classList.add('current');
+  const current = document.createElement("span");
+  current.classList.add("current");
   current.textContent = 1;
   fragment.appendChild(current);
 
-  const separation = document.createElement('span');
-  separation.textContent = '/';
-  separation.classList.add('separation');
+  const separation = document.createElement("span");
+  separation.textContent = "/";
+  separation.classList.add("separation");
   fragment.appendChild(separation);
 
-  const total = document.createElement('span');
-  total.classList.add('total');
-  total.textContent =item.length;
+  const total = document.createElement("span");
+  total.classList.add("total");
+  total.textContent = item.length;
   fragment.appendChild(total);
 
-  const getTextArea = document.querySelector('.mainvisual__textarea');
+  const getTextArea = document.querySelector(".mainvisual__textarea");
   getTextArea.appendChild(pagenation).appendChild(fragment);
 }
 
 function pagenationCountDown() {
-  const pagenationCurrent = document.querySelector('.current');
-  const pagenationTotal = Number(document.querySelector('.total').textContent);
+  const pagenationCurrent = document.querySelector(".current");
+  const pagenationTotal = Number(document.querySelector(".total").textContent);
   let currentNum = Number(pagenationCurrent.textContent);
 
-  if(currentNum <= pagenationTotal && currentNum !== 1){
+  if (currentNum <= pagenationTotal && currentNum !== 1) {
     pagenationCurrent.textContent = currentNum -= 1;
   }
 }
 
-function pagenationCountUp(){
-  const pagenationCurrent = document.querySelector('.current');
-  const pagenationTotal = Number(document.querySelector('.total').textContent);
+function pagenationCountUp() {
+  const pagenationCurrent = document.querySelector(".current");
+  const pagenationTotal = Number(document.querySelector(".total").textContent);
   let currentNum = Number(pagenationCurrent.textContent);
 
-  if(currentNum <= pagenationTotal && currentNum !== 5){
+  if (currentNum <= pagenationTotal && currentNum !== 5) {
     pagenationCurrent.textContent = currentNum += 1;
   }
 }
 
-function ImageNextSlide(e){
-  const slideImage = document.querySelectorAll('.slider__item');
-  const activeSlide = document.querySelector('.is-active');
+function ImageNextSlide(e) {
+  const slideImage = document.querySelectorAll(".slider__item");
+  const activeSlide = document.querySelector(".is-active");
   const nextSlide = activeSlide.nextElementSibling;
-  const nextSlideIndex = nextSlide.getAttribute('data-slide-index');
-  if(nextSlideIndex == slideImage.length){
-    nextSlide.classList.add('is-active');
-    activeSlide.classList.remove('is-active');
+  const nextSlideIndex = Number(nextSlide.getAttribute("data-slide-index"));
+  if (nextSlideIndex === slideImage.length) {
+    nextSlide.classList.add("is-active");
+    activeSlide.classList.remove("is-active");
     e.disabled = true;
-  }else{
+  } else {
     e.previousElementSibling.disabled = false;
-    nextSlide.classList.add('is-active');
-    activeSlide.classList.remove('is-active');
+    nextSlide.classList.add("is-active");
+    activeSlide.classList.remove("is-active");
   }
 }
 
 function ImagePrevSlide(e) {
-  const activeSlide = document.querySelector('.is-active');
+  const activeSlide = document.querySelector(".is-active");
   const prevSlide = activeSlide.previousElementSibling;
-  const prevSlideIndex = prevSlide.getAttribute('data-slide-index');
+  const prevSlideIndex = Number(prevSlide.getAttribute("data-slide-index"));
 
-  if(prevSlideIndex == 1){
-    prevSlide.classList.add('is-active');
-    activeSlide.classList.remove('is-active');
+  if (prevSlideIndex === 1) {
+    prevSlide.classList.add("is-active");
+    activeSlide.classList.remove("is-active");
     e.disabled = true;
-  }else{
+  } else {
     e.nextElementSibling.disabled = false;
-    prevSlide.classList.add('is-active');
-    activeSlide.classList.remove('is-active');
+    prevSlide.classList.add("is-active");
+    activeSlide.classList.remove("is-active");
   }
 }
 
-function callImageData(){
-  return new Promise(resolve => {
+function callImageData() {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(fetchImageData())
-    },3000);
-  })
+      resolve(fetchImageData());
+    }, 3000);
+  });
 }
 
 async function fetchImageData() {
-  const SLIDE_DATE_URL = 'https://api.json-generator.com/templates/9tm12BO1y5Xx/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl&delay=1000';
+  const SLIDE_DATE_URL =
+    "https://api.json-generator.com/templates/9tm12BO1y5Xx/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl&delay=1000";
   const response = await fetch(SLIDE_DATE_URL);
-  if(response.ok){
+  if (response.ok) {
     const json = await response.json();
     return json;
-  }else{
+  } else {
     console.error(`${response.status}:${response.statusText}`);
-    createErrorMessage('サーバの通信に失敗しました')
+    displayErrorMassage("サーバの通信に失敗しました");
   }
 }
 
 async function callSlideContents() {
   showLoadingImage();
-  try{
+  try {
     const json = await callImageData();
     const slideImageData = json.slide;
 
     renderSlideImage(slideImageData);
     renderPagenation(slideImageData);
-
-  }finally{
+  } finally {
     removeLoading();
   }
 }
 
-function init(){
+function init() {
   renderSlideArea();
-  renderSlideButton()
+  renderSlideButton();
   callSlideContents();
   const prevButton = document.querySelector('[aria-label="previous"]');
   prevButton.disabled = true;
@@ -212,20 +210,16 @@ function init(){
 
 init();
 
-
-const prevButton = document.getElementById('js-button_prev');
-prevButton.addEventListener('click', (e) => {
-  if(!e.currentTarget.hasAttribute('disable')){
+const prevButton = document.getElementById("js-button_prev");
+prevButton.addEventListener("click", (e) => {
+  if (!e.currentTarget.hasAttribute("disable")) {
     pagenationCountDown();
     ImagePrevSlide(e.currentTarget);
-
   }
-})
+});
 
-const nextButton = document.getElementById('js-button_next');
-nextButton.addEventListener('click', (e) => {
+const nextButton = document.getElementById("js-button_next");
+nextButton.addEventListener("click", (e) => {
   pagenationCountUp();
   ImageNextSlide(e.currentTarget);
-})
-
-
+});
