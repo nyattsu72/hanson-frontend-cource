6719e6344692587bc53eea965652939c8a9cf5b3
nyattsu72 <seki72ko@gmail.com>
2022-05-21 22:43:01 +0900

    refactor:Make an image call in the init function. Added processing when there were 0 images.

--- lesson17/src/js/main.js
+++ lesson17/src/js/main.js
@@ -1,4 +1,6 @@
-function createElementWithClassName (element, name) {
+import { id } from "date-fns/locale";
+
+function createElementWithClassName(element, name) {
   const createElement = document.createElement(element);
   createElement.classList.add(name);
   return createElement;
@@ -28,15 +30,16 @@ function displayErrorMassage(error) {
 }
 
 function renderSlideArea() {
-  const slideImageArea = createElementWithClassName("div", "mainvisual__images__inner");
+  const slideImageArea = createElementWithClassName(
+    "div",
+    "mainvisual__images__inner"
+  );
   document.querySelector(".mainvisual__images").appendChild(slideImageArea);
 }
 
 function renderSlideImage(item) {
   const slideImageArea = document.querySelector(".mainvisual__images__inner");
-
   const slider = createElementWithClassName("ul", "slider");
-
   const fragment = document.createDocumentFragment();
   item.forEach((image, i) => {
     const sliderItem = createElementWithClassName("li", "slider__item");
@@ -67,7 +70,7 @@ function renderSlideButton() {
     button.textContent = `${element}`;
     button.setAttribute("aria-label", `${element}`);
     fragment.appendChild(button);
-  })
+  });
 
   const slideArea = document.querySelector(".mainvisual__images");
   slideArea.appendChild(buttonArea).appendChild(fragment);
@@ -75,11 +78,9 @@ function renderSlideButton() {
 
 function renderPagenation(item) {
   const pagenation = createElementWithClassName("div", "slider__pagination");
-
   const fragment = document.createDocumentFragment();
-
   const current = document.createElement("span");
-  current.id = 'js-current';
+  current.id = "js-current";
   current.textContent = 1;
   fragment.appendChild(current);
 
@@ -88,7 +89,7 @@ function renderPagenation(item) {
   fragment.appendChild(separation);
 
   const total = document.createElement("span");
-  total.id = 'js-total';
+  total.id = "js-total";
   total.textContent = item;
   fragment.appendChild(total);
 
@@ -96,17 +97,29 @@ function renderPagenation(item) {
   textArea.appendChild(pagenation).appendChild(fragment);
 }
 
+function addNoimage(){
+  const slideImageArea = document.querySelector(".mainvisual__images__inner");
+  const sliderImage = createElementWithClassName("img", "slider__image");
+  sliderImage.src = './images/slide-image01.png'
+  sliderImage.width = 800;
+  sliderImage.height = 800;
+  sliderImage.style.opacity = 1;
+  slideImageArea.appendChild(sliderImage);
+}
+
 function switchPagination(target) {
   const pagenationCurrent = document.getElementById("js-current");
-  const pagenationTotal = Number(document.getElementById("js-total").textContent);
+  const pagenationTotal = Number(
+    document.getElementById("js-total").textContent
+  );
   let currentNum = Number(pagenationCurrent.textContent);
 
-  const buttonPrev = target.getAttribute("aria-label") === 'previous';
-  const buttonNext = target.getAttribute("aria-label") === 'next';
+  const buttonPrev = target.getAttribute("aria-label") === "previous";
+  const buttonNext = target.getAttribute("aria-label") === "next";
 
   if (currentNum < pagenationTotal && buttonNext) {
-    pagenationCurrent.textContent = currentNum +=1;
-  }else if(currentNum <= pagenationTotal && buttonPrev){
+    pagenationCurrent.textContent = currentNum += 1;
+  } else if (currentNum <= pagenationTotal && buttonPrev) {
     pagenationCurrent.textContent = currentNum -= 1;
   }
 }
@@ -117,12 +130,14 @@ function switchImg(target) {
   active[target].classList.add("is-active");
 }
 
-function toggleButtonDisabled(){
+function toggleButtonDisabled() {
   const slideImages = [...document.getElementsByClassName("slider__item")];
   const activeImage = document.querySelector(".is-active");
   const currentIndex = Number(activeImage.dataset.slideIndex);
   const firstIndex = 1;
   const lastIndex = slideImages.length;
+  const prevButton = document.getElementById("js-button_previous");
+  const nextButton = document.getElementById("js-button_next");
   prevButton.disabled = currentIndex === firstIndex;
   nextButton.disabled = currentIndex === lastIndex;
 }
@@ -130,7 +145,13 @@ function toggleButtonDisabled(){
 function callImageData() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(fetchImageData("https://api.json-generator.com/templates/9tm12BO1y5Xx/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl&delay=1000"));
+      resolve(
+        fetchImageData(
+          //https://api.json-generator.com/templates/IZjWl012CAMD/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl : image = 0
+          //https://api.json-generator.com/templates/9tm12BO1y5Xx/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl&status=503 : 503
+          "https://api.json-generator.com/templates/9tm12BO1y5Xx/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl"
+        )
+      );
     }, 3000);
   });
 }
@@ -146,42 +167,41 @@ async function fetchImageData(URL) {
   }
 }
 
-async function getSlideImage() {
+async function init() {
   showLoadingImage();
+  renderSlideArea();
+  let slideImageData;
   try {
     const json = await callImageData();
-    return json.slide;
-
+    slideImageData = json.slide;
+    if(slideImageData.length > 0){
+      renderSlideImage(slideImageData);
+      renderPagenation(slideImageData.length);
+      renderSlideButton();
+      toggleButtonDisabled();
+      addSwitchButtonEvent();
+    }else{
+      addNoimage();
+    }
   } finally {
     removeLoading();
   }
 }
 
-async function addSlideImage(){
-  const slideImageData = await getSlideImage();
-  renderSlideImage(slideImageData);
-  renderPagenation(slideImageData.length);
-  toggleButtonDisabled()
-}
-
-function init() {
-  renderSlideArea();
-  addSlideImage();
-  renderSlideButton();
-}
-
 init();
 
-const prevButton = document.getElementById("js-button_previous");
-prevButton.addEventListener("click", (e) => {
-  switchPagination(e.currentTarget);
-  switchImg("previousElementSibling");
-  toggleButtonDisabled()
-});
 
-const nextButton = document.getElementById("js-button_next");
-nextButton.addEventListener("click", (e) => {
-  switchPagination(e.currentTarget);
-  switchImg("nextElementSibling");
-  toggleButtonDisabled()
-});
+function addSwitchButtonEvent(){
+  const prevButton = document.getElementById("js-button_previous");
+  const nextButton = document.getElementById("js-button_next");
+  prevButton.addEventListener("click", (e) => {
+    switchPagination(e.currentTarget);
+    switchImg("previousElementSibling");
+    toggleButtonDisabled();
+  });
+  nextButton.addEventListener("click", (e) => {
+    switchPagination(e.currentTarget);
+    switchImg("nextElementSibling");
+    toggleButtonDisabled();
+  });
+}
