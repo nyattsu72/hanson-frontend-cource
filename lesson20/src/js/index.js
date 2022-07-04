75133c54c7f9cffb70258b56e8348b83c8cabfcf
nyattsu72 <seki72ko@gmail.com>
2022-07-05 06:25:14 +0900

    add:Added userdata get function.

--- lesson20/src/js/index.js
+++ lesson20/src/js/index.js
@@ -1,6 +1,13 @@
 import { createAttributedElements } from './utiles/createAttributeWithEllement';
 import { showLoadingImage, removeLoading } from './modules/loading';
 
+function displayErrorMassage(error) {
+  const slideImageArea = document.getElementById("js-mainvisual-images-inner");
+  const textBox = createAttributedElements("p", "error-message");
+  textBox.textContent = error;
+  slideImageArea.appendChild(textBox);
+}
+
 const userItemName = {
   userID : "ID",
   userName : "名前",
@@ -26,4 +33,40 @@ function createTableItem (){
   });
 }
 
- createTableItem ()
+const USER_LISTS_URL = "https://api.json-generator.com/templates/2n667LPMsECB/data?access_token=b0154huvd1stffra1six9olbgg34r4zofcqgwzfl";
+
+async function fetchContents(endpoint){
+  const response = await fetch(endpoint);
+  if(response.ok){
+    const json = await response.json();
+    return json;
+  } else{
+    console.error(`${response.status}:${response.statusText}`)
+    displayErrorMassage("Internet Server Error");
+  }
+}
+
+const getUserLists = (sec) =>{
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve(fetchContents(USER_LISTS_URL));
+    },1000*sec);
+  })
+}
+
+async function init(){
+  showLoadingImage();
+  let userLists;
+  try{
+    const json = await getUserLists(3);
+    userLists = json.users;
+  }catch (error){
+    console.error(error);
+    displayErrorMassage(error);
+  }finally{
+    removeLoading();
+  }
+  console.log(userLists);
+}
+
+init();
